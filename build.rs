2036c2794d373b9f76099d2d001e3c23a593e2bf
Geoffroy Couprie <contact@geoffroycouprie.com>
2021-11-13 12:54:23 +0100

    build+release process

---
+++ build.rs
@@ -0,0 +1,28 @@
+use std::env;
+use std::fs::{self, File};
+use std::path::Path;
+use std::process;
+
+
+fn main() {
+    // OUT_DIR is set by Cargo and it's where any additional build artifacts
+    // are written.
+    let outdir = match env::var_os("OUT_DIR") {
+        Some(outdir) => outdir,
+        None => {
+            eprintln!(
+                "OUT_DIR environment variable not defined. \
+                 Please file a bug: \
+                 https://github.com/biscuit-auth/biscuit-cli/issues/new"
+            );
+            process::exit(1);
+        }
+    };
+    fs::create_dir_all(&outdir).unwrap();
+
+    let stamp_path = Path::new(&outdir).join("biscuit-stamp");
+    if let Err(err) = File::create(&stamp_path) {
+        panic!("failed to write {}: {}", stamp_path.display(), err);
+    }
+}
+
